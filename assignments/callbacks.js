36a56c56e6359e15c8bbcf63f897ec93dd642c96
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-08-19 22:07:02 -0400

    Completed callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,24 +41,51 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb = arr.length;
+  return cb;
 }
+
+console.log(getLength(items));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb = arr[arr.length - 1]
+  return cb;
 }
+
+console.log(last(items));
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb = x + y;
+  return cb;
 }
+
+console.log(sumNums(2, 3))
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb = x * y;
+  return cb;
 }
+
+console.log(multiplyNums(2, 3))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      cb = true;
+    } else {
+      cb = false;
+    }
+  }
+  return cb;
 }
+
+console.log(contains('Daisy', items))
 
 /* STRETCH PROBLEM */
 
